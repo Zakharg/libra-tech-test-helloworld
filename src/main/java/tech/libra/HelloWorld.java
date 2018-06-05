22c752539dad1e3c6e3c2ff88628ad2b8950250b
George Zakharov <george.zakharov@libra.tech>
2018-06-05 09:46:54 -0400

    Added A Jenkisfile

--- src/main/java/tech/libra/HelloWorld.java
+++ src/main/java/tech/libra/HelloWorld.java
@@ -6,11 +6,8 @@ import tech.libra.jenkins.test.calculator.*;
 public class HelloWorld {
 	
 	private static Calculator calculator = new CalculatorImpl();
-	private static String str;
 	
 	public static void main (String []args) {
-		
-		str.equals("word");
 		
 		System.out.println(calculator.add(5,9));
 		
