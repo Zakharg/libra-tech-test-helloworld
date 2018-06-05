30aba4253608a58a6f708c14be3078c07a5ef9b2
George Zakharov <george.zakharov@libra.tech>
2018-06-05 09:08:25 -0400

    Pom file

--- src/main/java/tech/libra/HelloWorld.java
+++ src/main/java/tech/libra/HelloWorld.java
@@ -1,8 +1,18 @@
 package tech.libra;
 
-public class HelloWorld {
-	public static void main (String []args) {
-		System.out.println("hello");
-	}
+import tech.libra.jenkins.test.calculator.*;
 
+
+public class HelloWorld {
+	
+	private static Calculator calculator = new CalculatorImpl();
+	private static String str;
+	
+	public static void main (String []args) {
+		
+		str.equals("word");
+		
+		System.out.println(calculator.add(5,9));
+		
+	}
 }
